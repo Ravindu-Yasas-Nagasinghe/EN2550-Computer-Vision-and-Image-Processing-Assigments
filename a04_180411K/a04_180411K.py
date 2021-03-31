73692c32aa480ac81c89de3e3371a9839d96be79
Ravindu-Yasas-Nagasinghe <56619402+Ravindu-Yasas-Nagasinghe@users.noreply.github.com>
2021-03-31 21:12:17 +0530

    Update a04_180411K.py
    Uploaded the code for part 2. Coded for  a two-layer fully connected network with H = 200 hidden nodes and plotted weights matrix W as 10 images and plotted losses, accuracy and learning rate.

--- a04_180411K/a04_180411K.py
+++ a04_180411K/a04_180411K.py
@@ -114,7 +114,7 @@ for i in range(w1.shape[1]):
     reshapen=np.reshape(w1[:,i]*255,(32,32,3))
     normalized=cv.normalize(reshapen, None, 0, 255, cv.NORM_MINMAX, cv.CV_8U)
     images.append(normalized)
-fig,ax=plt.subplots(2,5,figsize=(30,10))
+fig,ax=plt.subplots(2,5,figsize=(40,10))
 for i in range(2):
     for j in range(5):
         ax[i,j].imshow(images[i*5+j],vmin=0,vmax=255)
@@ -122,3 +122,104 @@ for i in range(2):
         ax[i,j].set_yticks([])
         ax[i,j].set_title(classes[i*5+j])
 plt.show()
+
+#part 2
+
+# Function for two layer dense network
+def layer_2(x_train,y_train,x_test,y_test,Din,lr,lr_decay,H,reg,K,Ntr,Nte):
+    loss_history = []
+    loss_history_test = []
+    train_acc_history = []
+    val_acc_history = []
+    lr_array =[]
+    seed = 0
+    rng = np.random.default_rng(seed=seed)
+    batch_size=Ntr
+
+    #initializing weight and bias arrays
+    std=1e-5
+    w1 = std*np.random.randn(Din, H)
+    b1 = np.zeros(H)
+    w2 = std*np.random.randn(H, K)
+    b2 = np.zeros(K)
+
+    for t in range(iterations):
+        indices = np.random.choice(Ntr,batch_size)
+        
+        rng.shuffle(indices)# To avoid overfitting shuffle the training data set
+        x=x_train[indices]
+        y=y_train[indices]
+
+        #forward pass
+        h=1/(1+np.exp(-(x.dot(w1)+b1)))
+        h_test=1/(1+np.exp(-((x_test).dot(w1)+b1)))
+        y_pred=h.dot(w2)+b2
+        y_pred_test=h_test.dot(w2)+b2
+
+        # calculating the training and testing loss
+        training_loss=(1/batch_size)*(np.square(y_pred-y)).sum()+reg*(np.sum(w1*w1)+np.sum(w2*w2))
+        testing_loss=(1/batch_size)*(np.square(y_pred_test-y_test)).sum()+reg*(np.sum(w1*w1)+np.sum(w2*w2))
+        loss_history.append(training_loss)
+        loss_history_test.append(testing_loss)
+        
+        # calculating trainning and testing accuracies
+        train_accuracy=1-(1/(10*Ntr))*(np.abs(np.argmax(y_train,axis=1)-np.argmax(y_pred,axis=1))).sum()
+        train_acc_history.append(train_accuracy)
+
+        test_accuracy=1-(1/(10*Nte))*(np.abs(np.argmax(y_test,axis=1)-np.argmax(y_pred_test,axis=1))).sum()
+        val_acc_history.append(test_accuracy)
+
+        if t%10 == 0:
+            print('epoch %d/%d: loss= %f || , test loss= %f ||, train accuracy= %f ||, test accuracy= %f ||, learning rate= %f ||' % (t,iterations,training_loss,testing_loss,train_accuracy,test_accuracy,lr))
+
+        # Backward pass
+        dy_pred=(1./batch_size)*2.0*(y_pred-y)
+        dw2=h.T.dot(dy_pred)+reg*w2
+        db2=dy_pred.sum(axis=0)
+        dh=dy_pred.dot(w2.T)
+        dw1=x.T.dot(dh*h*(1-h))+reg*w1
+        db1=(dh*h*(1-h)).sum(axis=0)
+        # updating parameters
+        w1-=lr*dw1 
+        w2-=lr*dw2
+        b1-=lr*db1
+        b2-=lr*db2
+        lr_array.append(lr)
+        lr*=lr_decay
+    return w1,b1,w2,b2,loss_history,loss_history_test,train_acc_history,val_acc_history,lr_array
+x_train_2layer,y_train_2layer,x_test_2layer,y_test_2layer,K,Din,Ntr,Nte=preprocessing(normalize=False,reshape=True) 
+iterations = 300#gradient descent iterations
+lr = 1.4e-2#learning rate
+lr_decay= 0.999
+reg = 5e-6
+H=200 #hidden layer nodes  
+w1_2layer,b1_2layer,w2_2layer,b2_2layer,loss_history_2layer,loss_history_test_2layer,train_acc_history_2layer,val_acc_history_2layer,lr_array_2layer=layer_2(x_train_2layer,y_train_2layer,x_test_2layer,y_test_2layer,Din,lr,lr_decay,H,reg,K,Ntr,Nte)
+
+
+# ploting the graphs of training and testing losses, training and testing accuracies and learning rate
+fig, axes  = plt.subplots(1,5,figsize=(50,10))
+titles = {"Training Loss":loss_history_2layer, "testing loss":loss_history_test_2layer,"Training Accuracy":train_acc_history_2layer,
+         "testing Accuracy": val_acc_history_2layer, "Learning Rate":lr_array_2layer}
+place = 0
+for key in titles.keys():
+    axes[place].plot(titles[key])
+    axes[place].set_title(key)
+    place+=1
+plt.show()
+#plotting the weight matrix W as 10 images
+images=[]
+classes=['airplane','automobile','bird','cat','deer','dog','frog','horse','ship','truck']#classes of cifar-10
+for i in range(w1_2layer.shape[1]):
+    reshapen=np.reshape(w1_2layer[:,i]*255,(32,32,3))
+    normalized=cv.normalize(reshapen, None, 0, 255, cv.NORM_MINMAX, cv.CV_8U)
+    images.append(normalized)
+fig,ax=plt.subplots(2,5,figsize=(40,10))
+for i in range(2):
+    for j in range(5):
+        ax[i,j].imshow(images[i*5+j],vmin=0,vmax=255)
+        ax[i,j].set_xticks([])
+        ax[i,j].set_yticks([])
+        ax[i,j].set_title(classes[i*5+j])
+plt.show()
+
+#part3
