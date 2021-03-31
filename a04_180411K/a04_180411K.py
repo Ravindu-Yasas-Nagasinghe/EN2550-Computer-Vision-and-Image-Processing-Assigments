7b744e2dce33ebc62080f3ddba9077aee2296c61
Ravindu-Yasas-Nagasinghe <56619402+Ravindu-Yasas-Nagasinghe@users.noreply.github.com>
2021-03-31 22:18:04 +0530

    Update a04_180411K.py
    commented on some important code parts

--- a04_180411K/a04_180411K.py
+++ a04_180411K/a04_180411K.py
@@ -3,6 +3,8 @@ from tensorflow import keras
 import numpy as np
 import matplotlib.pyplot as plt
 import cv2 as cv
+from tensorflow.keras import layers,models
+
 
 # Defining function for data preprocessing
 def preprocessing(normalize,reshape):
@@ -10,12 +12,13 @@ def preprocessing(normalize,reshape):
     (x_train, y_train), (x_test, y_test) = keras.datasets.cifar10.load_data()
 
     K = len(np.unique(y_train)) # no.of Classes 
-    Ntr = x_train.shape[0] # Number of training data
-    Nte = x_test.shape[0] # Number of testing data
+    Ntr = x_train.shape[0] # Number of training data=50,000
+    Nte = x_test.shape[0] # Number of testing data=10,000
     Din = 3072 # CIFAR10 = 3072 = 32x32x3
 
     if normalize:
         x_train, x_test = x_train / 255.0, x_test / 255.0# Normalize pixel values
+
     # Center the pixel values
     mean_image = np.mean(x_train, axis=0)
     x_train = x_train - mean_image
@@ -23,11 +26,11 @@ def preprocessing(normalize,reshape):
     y_train = tf.keras.utils.to_categorical(y_train, num_classes=K)
     y_test = tf.keras.utils.to_categorical(y_test, num_classes=K)
 
-    #flatterning the input images and changing the data type
+    #flatterning the input images
     if reshape:
         x_train = np.reshape(x_train,(Ntr,Din))
         x_test = np.reshape(x_test,(Nte,Din))
-    
+    # Changing the data types
     x_train = x_train.astype('float32')
     x_test = x_test.astype('float32')
     return x_train,y_train,x_test,y_test,K,Din,Ntr,Nte
@@ -56,7 +59,7 @@ def layer1LinearClassifier(x_train,y_train,x_test,y_test,K,Din,lr,lr_decay,reg,N
         x=x_train[indices]
         y=y_train[indices]
 
-        # forward pass
+        # forward propagation
         y_pred=x.dot(w1)+b1
         y_pred_test=x_test.dot(w1)+b1
 
@@ -74,30 +77,32 @@ def layer1LinearClassifier(x_train,y_train,x_test,y_test,K,Din,lr,lr_decay,reg,N
         val_acc_history.append(test_accuracy)
 
         if t%10 == 0:
-            print('epoch %d/%d: train loss= %f || ,test loss= %f ||,train accuracy= %f ||, test accuracy= %f ||, learning rate= %f ||' % (t,iterations,train_loss,test_loss,train_accuracy,test_accuracy,lr))
+            print('epoch %d/%d: train loss= %f || ,test loss= %f ||,train accuracy= %f ||, test accuracy= %f ||, learning rate= %f ||' 
+            % (t,iterations,train_loss,test_loss,train_accuracy,test_accuracy,lr))
 
-        # Backward pass
+        # Backward propagation
         dy_pred=(1./batch_size)*2.0*(y_pred-y)#partial deravative of L w.r.t y_pred
         dw1=x.T.dot(dy_pred)+reg*w1
         db1=dy_pred.sum(axis=0)
 
-        # updating parameters
+        
         w1-=lr*dw1#update weight matrix
         b1-=lr*db1#update bias matrix
         lr_array.append(lr)
-        lr*=lr_decay#decaying learning rate
+        lr*=lr_decay#decaying the learning rate
     return w1,b1,loss_history,loss_history_testing,train_acc_history,val_acc_history,lr_array
 
 
-#defining parameters and running the linear classifier 
+#defining parameters 
 iterations = 300#gradient descent iterations
 lr = 1.4e-2#learning rate
 lr_decay= 0.999
 reg = 5e-6 #lambda=regularization constant of the loss function
 x_train,y_train,x_test,y_test,K,Din,Ntr,Nte=preprocessing(normalize=True,reshape=True)
+ #Run the linear classifier
 w1,b1,loss_history,loss_history_test,train_acc_history,val_acc_history,lr_array=layer1LinearClassifier(x_train,y_train,x_test,y_test,K,Din,lr,lr_decay,reg,Ntr,Nte)
 
-# ploting the graphs of trining and testing losses, training and testing accuracies and learning rate
+# ploting the graphs of training and testing losses, training and testing accuracies and learning rate
 fig, axes  = plt.subplots(1,5)
 titles = {"Training Loss":loss_history, "testing loss":loss_history_test,"Training Accuracy":train_acc_history,
          "testing Accuracy": val_acc_history, "Learning Rate":lr_array}
@@ -114,7 +119,7 @@ for i in range(w1.shape[1]):
     reshapen=np.reshape(w1[:,i]*255,(32,32,3))
     normalized=cv.normalize(reshapen, None, 0, 255, cv.NORM_MINMAX, cv.CV_8U)
     images.append(normalized)
-fig,ax=plt.subplots(2,5,figsize=(40,10))
+fig,ax=plt.subplots(2,5,figsize=(30,10))
 for i in range(2):
     for j in range(5):
         ax[i,j].imshow(images[i*5+j],vmin=0,vmax=255)
@@ -123,7 +128,6 @@ for i in range(2):
         ax[i,j].set_title(classes[i*5+j])
 plt.show()
 
-#part 2
 
 # Function for two layer dense network
 def layer_2(x_train,y_train,x_test,y_test,Din,lr,lr_decay,H,reg,K,Ntr,Nte):
@@ -136,10 +140,11 @@ def layer_2(x_train,y_train,x_test,y_test,Din,lr,lr_decay,H,reg,K,Ntr,Nte):
     rng = np.random.default_rng(seed=seed)
     batch_size=Ntr
 
-    #initializing weight and bias arrays
     std=1e-5
+    #initializing weight and bias matrices for hidden layer
     w1 = std*np.random.randn(Din, H)
     b1 = np.zeros(H)
+    #initializing weight and bias matrices for final layer
     w2 = std*np.random.randn(H, K)
     b2 = np.zeros(K)
 
@@ -150,7 +155,7 @@ def layer_2(x_train,y_train,x_test,y_test,Din,lr,lr_decay,H,reg,K,Ntr,Nte):
         x=x_train[indices]
         y=y_train[indices]
 
-        #forward pass
+        #forward propagation
         h=1/(1+np.exp(-(x.dot(w1)+b1)))
         h_test=1/(1+np.exp(-((x_test).dot(w1)+b1)))
         y_pred=h.dot(w2)+b2
@@ -168,30 +173,33 @@ def layer_2(x_train,y_train,x_test,y_test,Din,lr,lr_decay,H,reg,K,Ntr,Nte):
 
         test_accuracy=1-(1/(10*Nte))*(np.abs(np.argmax(y_test,axis=1)-np.argmax(y_pred_test,axis=1))).sum()
         val_acc_history.append(test_accuracy)
-
+        # Print for every 10 iterations
         if t%10 == 0:
-            print('epoch %d/%d: loss= %f || , test loss= %f ||, train accuracy= %f ||, test accuracy= %f ||, learning rate= %f ||' % (t,iterations,training_loss,testing_loss,train_accuracy,test_accuracy,lr))
+            print('epoch %d/%d: loss= %f || , test loss= %f ||, train accuracy= %f ||, test accuracy= %f ||, learning rate= %f ||' 
+            % (t,iterations,training_loss,testing_loss,train_accuracy,test_accuracy,lr))
 
-        # Backward pass
+        # Backward propagation
         dy_pred=(1./batch_size)*2.0*(y_pred-y)
         dw2=h.T.dot(dy_pred)+reg*w2
         db2=dy_pred.sum(axis=0)
         dh=dy_pred.dot(w2.T)
         dw1=x.T.dot(dh*h*(1-h))+reg*w1
         db1=(dh*h*(1-h)).sum(axis=0)
-        # updating parameters
+        #update weight matrices
         w1-=lr*dw1 
         w2-=lr*dw2
+        #update bias matrices
         b1-=lr*db1
         b2-=lr*db2
         lr_array.append(lr)
-        lr*=lr_decay
+        lr*=lr_decay#decaying the learning rate
     return w1,b1,w2,b2,loss_history,loss_history_test,train_acc_history,val_acc_history,lr_array
-x_train_2layer,y_train_2layer,x_test_2layer,y_test_2layer,K,Din,Ntr,Nte=preprocessing(normalize=False,reshape=True) 
+x_train_2layer,y_train_2layer,x_test_2layer,y_test_2layer,K,Din,Ntr,Nte=preprocessing(normalize=False,reshape=True)
+#Remove the normalization. Otherwise the model will not learn 
 iterations = 300#gradient descent iterations
 lr = 1.4e-2#learning rate
 lr_decay= 0.999
-reg = 5e-6
+reg = 5e-6#lambda
 H=200 #hidden layer nodes  
 w1_2layer,b1_2layer,w2_2layer,b2_2layer,loss_history_2layer,loss_history_test_2layer,train_acc_history_2layer,val_acc_history_2layer,lr_array_2layer=layer_2(x_train_2layer,y_train_2layer,x_test_2layer,y_test_2layer,Din,lr,lr_decay,H,reg,K,Ntr,Nte)
 
@@ -221,5 +229,6 @@ for i in range(2):
         ax[i,j].set_yticks([])
         ax[i,j].set_title(classes[i*5+j])
 plt.show()
+
 
 #part3
